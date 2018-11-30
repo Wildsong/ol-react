f85382aeeb539b6e2496833d2b20e7d534bbf874
Brian H Wilson <bwilson@co.clatsop.or.us>
2018-11-30 09:31:25 -0800

    Adding more tests

--- index.js
+++ index.js
@@ -3,24 +3,40 @@
 import React from 'react';
 import { render } from 'react-dom';
 import {Map, View, layer, source, Feature, geom} from './src';
+import apiKeys from './apikeys';
 
 const App = () => (
     <div>
-    <p>Here's my declarative map app</p>
-
+    <h2>Source tests</h2>
+    <h3>OpenStreetMap</h3>
+    *Also tests Vector source by drawing a red box around Null Island
     <Map view=<View resolution={10000} center={[0, 0]}/>>
-      <layer.Tile>
-        <source.OSM />
-      </layer.Tile>
-      <layer.Vector>
-        <source.Vector>
-          <Feature style={{stroke: {color: [255, 0, 0, 1]}}}>
-            <geom.LineString>
-              {[[0, 0], [100000, 0], [100000, 100000], [0, 100000]]}
-            </geom.LineString>
-          </Feature>
-        </source.Vector>
-      </layer.Vector>
+        <layer.Tile>
+            <source.OSM />
+        </layer.Tile>
+        <layer.Vector>
+            <source.Vector>
+                <Feature style={{stroke: {color: [255, 0, 0, 1]}}}>
+    {/*            <geom.LineString>
+                  {[[0, 0], [100000, 0], [100000, 100000], [0, 100000], [0, 0]]}
+                </geom.LineString>*/}
+                </Feature>
+            </source.Vector>
+        </layer.Vector>
+    </Map>
+
+    <h3>ArcGIS image source</h3>
+    <Map view=<View resolution={10000} center={[0, 0]}/>>
+        <layer.Tile>
+            <source.ImageArcGISRest />
+        </layer.Tile>
+    </Map>
+
+    <h3>BingMaps</h3>
+    <Map view=<View resolution={10000} center={[0, 0]}/>>
+        <layer.Tile>
+            <source.BingMaps apiKey={apiKeys['BingMaps']} />
+        </layer.Tile>
     </Map>
 
     </div>
@@ -29,5 +45,3 @@ const App = () => (
 render(
     <App />, document.getElementById("app")
 );
-
-console.log('index.js loaded');
