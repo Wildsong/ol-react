15123a33e96a2221d416509773db5ea2fbfd8ad8
Brian H Wilson <brian@wildsong.biz>
2019-01-06 16:10:09 -0800

    oh who knows

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // App.js ol-react
 //
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import {BrowserRouter, Link, Route, Redirect, Switch} from 'react-router-dom'
 import {
@@ -13,16 +13,14 @@ import {
     NavLink,
     Button
 } from 'reactstrap'
-import {Example1, Example2, Example3, Example4} from './examples'
-import Slider from 'rc-slider'
+import {Example1, Example2, Example3} from './examples'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
+import './App.css';
 
 const SampleMap1 = () => ( <Example1 title="Sample Map 1" /> );
 const SampleMap2 = () => ( <Example2 title="Sample Map 2" /> );
 const SampleMap3 = () => ( <Example3 title="Sample Map 3" /> );
-const SampleMap4 = () => ( <Example4 title="Sample Map 4" /> );
 
 class PrimaryLayout extends Component {
     constructor(props) {
@@ -34,8 +32,7 @@ class PrimaryLayout extends Component {
 
     render(props) {
         return (
-            <div>
-                <Slider />
+            <Fragment>
                 <Navbar color="light" light expand="md">
                   <NavbarBrand href="/">
                     <span id="sitelogo"></span>
@@ -54,9 +51,6 @@ class PrimaryLayout extends Component {
                     <NavItem>
                       <NavLink href="/sample3">SampleMap3</NavLink>
                     </NavItem>
-                    <NavItem>
-                      <NavLink href="/sample4">SampleMap4</NavLink>
-                    </NavItem>
                     </Nav>
                   </Collapse>
                 </Navbar>
@@ -65,9 +59,8 @@ class PrimaryLayout extends Component {
                 <Route exact path="/" component={SampleMap1} />
                 <Route path="/sample2" component={SampleMap2} />
                 <Route path="/sample3" component={SampleMap3} />
-                <Route path="/sample4" component={SampleMap4} />
                 </Switch>
-            </div>
+            </Fragment>
         );
     }
 }
