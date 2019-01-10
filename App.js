eddb58d4f67f64804e71ad496325e0afe3abd629
Brian H Wilson <bwilson@co.clatsop.or.us>
2019-01-09 16:58:59 -0800

    added example 4

--- App.js
+++ App.js
@@ -13,14 +13,15 @@ import {
     NavLink,
     Button
 } from 'reactstrap'
-import {Example1, Example2, Example3} from './examples'
+import {Example1, Example2, Example3, Example4} from './examples'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
-const SampleMap1 = () => ( <Example1 title="Sample Map 1" /> );
-const SampleMap2 = () => ( <Example2 title="Sample Map 2" /> );
-const SampleMap3 = () => ( <Example3 title="Sample Map 3" /> );
+const SampleMap1 = () => ( <Example1 title="Sample 1" /> );
+const SampleMap2 = () => ( <Example2 title="Sample 2" /> );
+const SampleMap3 = () => ( <Example3 title="Sample 3" /> );
+const SampleMap4 = () => ( <Example4 title="Sample 4" /> );
 
 class PrimaryLayout extends Component {
     constructor(props) {
@@ -43,13 +44,16 @@ class PrimaryLayout extends Component {
                   <Collapse isOpen={this.state.isOpen} navbar>
                     <Nav className="ml-auto" navbar>
                     <NavItem>
-                      <NavLink href="/">SampleMap1</NavLink>
+                      <NavLink href="/">Sample 1</NavLink>
                     </NavItem>
                     <NavItem>
-                      <NavLink href="/sample2">SampleMap2</NavLink>
+                      <NavLink href="/sample2">Sample 2</NavLink>
                     </NavItem>
                     <NavItem>
-                      <NavLink href="/sample3">SampleMap3</NavLink>
+                      <NavLink href="/sample3">Sample 3</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink href="/sample4">Sample 4</NavLink>
                     </NavItem>
                     </Nav>
                   </Collapse>
@@ -59,6 +63,7 @@ class PrimaryLayout extends Component {
                 <Route exact path="/" component={SampleMap1} />
                 <Route path="/sample2" component={SampleMap2} />
                 <Route path="/sample3" component={SampleMap3} />
+                <Route path="/sample4" component={SampleMap4} />
                 </Switch>
             </Fragment>
         );
