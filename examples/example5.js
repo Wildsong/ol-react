60fbd8b675e42c299dd6cddf9a822eb9113690fb
Brian H Wilson <bwilson@co.clatsop.or.us>
2019-01-23 17:17:30 -0800

    Initial geocode! almost works!

--- examples/example5.js
+++ examples/example5.js
@@ -12,23 +12,42 @@ const astoria_wm = transform([-123.834,46.187], wgs84,wm)
 
 const taxlotFeatureServer = "https://cc-gis.clatsop.co.clatsop.or.us/arcgis/rest/services/Assessment_and_Taxation/Taxlots_3857/FeatureServer/"
 
+import jquery from 'jquery'
+
+
 class Geocode extends Component {
     static propTypes = {
     }
     state = {
-        city: ''
+        address: ''
     }
 
     onTextChange = (evt) => {
         this.setState({
-            city: evt.target.value
+            address: evt.target.value
         });
         return true;
     }
 
+    geocode(address) {
+        const geocodeServer = "https://nominatim.openstreetmap.org/search?"
+        jquery.ajax({
+            url: geocodeServer + "q=" + address + "&format=xml",
+            dataType: 'xml',
+            success: function(response) {
+                if (response.error) {
+                    console.log(response.error.message + response.error.details);
+                } else {
+                    console.log("Response: ", response);
+                }
+            }
+        });
+    }
+
     onTextSubmit = (evt) => {
         if (evt.key == 'Enter') {
-            console.log("Go to:", this.state.city);
+            console.log("Go to:", this.state.address);
+            this.geocode(this.state.address)
             return true; // key handled!
         }
         return false;
@@ -37,7 +56,7 @@ class Geocode extends Component {
     render() {
         return (
             <>
-                <input type="text" name="city" placeholder="City" value={ this.state.city }
+                <input type="text" name="address" placeholder="Address" value={ this.state.address }
                     onChange={ this.onTextChange }
                     onKeyDown={ this.onTextSubmit }
                 />
