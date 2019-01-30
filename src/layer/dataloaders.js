68f61063248a0b89670e681cc919c492201c83e3
Brian H Wilson <bwilson@co.clatsop.or.us>
2019-01-29 16:06:52 -0800

    Switched from jquery to jsonp which might or might not be an improvement.

--- src/layer/dataloaders.js
+++ src/layer/dataloaders.js
@@ -1,8 +1,7 @@
 import { EsriJSON } from 'ol/format'
 
-// THIS IS TEMPORARY!!
-// only using ajax not accessing DOM so should be okay for now.
-import jquery from 'jquery'
+// Can't use axios here because it does not support jsonp
+import jsonp from 'jsonp'
 
 let esrijsonFormat = new EsriJSON();
 
@@ -19,20 +18,17 @@ export const DataLoader = (sourceType, url, source) => {
                                  + ',"xmax":' + extent[2] + ',"ymax":' + extent[3])
                                  + '&geometryType=esriGeometryEnvelope&outFields=*';
         //console.log("esriLoader_", fsurl, extent, resolution, projection);
-        jquery.ajax({
-            url: fsurl,
-            dataType: 'jsonp',
-            success: function(response) {
-                if (response.error) {
-                    console.log(response.error.message + response.error.details + ' IS IT SHARED? I can\'t do auth!');
-                } else {
-                    let features = esrijsonFormat.readFeatures(response, {
-                            featureProjection: projection
-                    });
-                    //console.log("Adding features.", features.length, source);
-                    if (features.length > 0) {
-                        source.addFeatures(features);
-                    }
+        jsonp(fsurl, null, (err, data) => {
+            if (err) {
+                console.log("DataLoader() error:", error);
+            } else {
+                //console.log("DataLoader() response", data)
+                let features = esrijsonFormat.readFeatures(data, {
+                    featureProjection: projection
+                });
+                if (features.length > 0) {
+                    console.log("DataLoader() Adding ", features.length);
+                    source.addFeatures(features);
                 }
             }
         });
