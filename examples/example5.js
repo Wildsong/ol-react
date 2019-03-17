fd1fa1464cca584c1e1fa594f1f7037091dc70fc
Brian H Wilson <brian@wildsong.biz>
2019-03-17 10:29:49 -0700

    added issue #2 test

--- examples/example5.js
+++ examples/example5.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { transform } from 'ol/proj'
-import {Map, View, Feature, geom, layer} from '../src'
-
+import { Map, View, layer } from '../src'
+import { Point } from 'ol/geom'
+import { Feature } from 'ol'
+import { Vector as VectorSource } from 'ol/source'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../App.css'
 
 import { wgs84, wm, astoria_ll } from '../src/utils'
 
-const taxlotFeatureServer = "https://cc-gis.clatsop.co.clatsop.or.us/arcgis/rest/services/Assessment_and_Taxation/Taxlots_3857/FeatureServer/"
+// CC service only works inside firewall
+// const taxlots = "https://cc-gis.clatsop.co.clatsop.or.us/arcgis/rest/services/Assessment_and_Taxation/Taxlots_3857/FeatureServer/"
+
+// To generate this URL,
+// go into GeoServer Layer Preview,
+// and in All Formats, select "WFS GeoJSON(JSONP)"
+// then paste here and
+// clip off the outputFormat and maxFeatures attributes (maxFeatures=50&outputFormat=text%2Fjavascript
+const taxlots = 'http://maps.wildsong.biz/geoserver/clatsop-wfs/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=clatsop-wfs%3Ataxlots'
 
 export default class Example5 extends Component {
     static propTypes = {
@@ -77,11 +87,33 @@ export default class Example5 extends Component {
         e.preventDefault();
     }
 
+    handleEvent = (e) => {
+        console.log("Map.handleEvent", e.map)
+        //e.stopPropagation(); // this stops draw interaction
+    }
+
     render() {
         let polyStyle = {
             stroke: {color: [0, 0, 0, 1], width:4},
             fill: {color: [255, 0, 0, .250]},
         };
+
+        var style = {
+            image: {
+                type: 'circle',
+                radius: 4,
+                fill: { color: [100,100,100, 0.5] },
+                stroke: { color: 'green', width: 1 }
+            }
+        }
+        var point = new Point(
+            transform([-1.49, 49], 'EPSG:4326', 'EPSG:3857')
+        );
+        var pointFeature = new Feature(point);
+        var vectorSource = new VectorSource({
+            projection: 'EPSG:4326'
+        });
+        vectorSource.addFeatures([pointFeature]);
 
         let ll = transform([this.state.lon, this.state.lat], wgs84, wm);
 
@@ -89,14 +121,16 @@ export default class Example5 extends Component {
             <>
                 <h2>{ this.props.title }</h2>
 
+<em>2019-03-17 I AM WORKING ON THIS EXAMPLE RIGHT NOW SO IT'S BUGGY!</em>
+
                 <p>
                 This form demonstrates that I can keep the map center<br />
                 and zoom in component state and update the map using setState.
                 </p>
+                { this.state.lats }, { this.state.lons } { this.state.zoom }
                 <form onSubmit={ this.submit }>
-                  <input name="lats"  value={ this.state.lats }  onChange={ this.change }/>
-                  <input name="lons"  value={ this.state.lons }  onChange={ this.change }/>
-                  <input type="submit"/>
+                  <input name="addr"  onChange={ this.change }/>
+                  <input type="submit" value="Geocode" />
                 </form>
                 Zoom
                 <button name="zoomin"  onClick={ this.click }>+</button>
@@ -111,14 +145,19 @@ export default class Example5 extends Component {
                         center={ ll }
                         rotation={ this.state.rotation }
                     />
+                    onMoveEnd={ this.handleEvent }
                 >
                     <layer.Tile source="OSM" />
                     <layer.Vector name="Taxlots"
-                        source="esrijson"
-                        url={ taxlotFeatureServer }
+                        source="geojson"
+                        url={ taxlots }
                         style={ polyStyle }
                     />
+
+                    test for issue #2
+                    <layer.Vector name="Display" source={vectorSource} style={style}/>
                 </Map>
+
 
                 <table>
                     { this.state.geocoderesults.map(gc =>
