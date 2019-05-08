0fdff88e08295223429d5be6b3dfe2ce6c85f7e4
Brian H Wilson <bwilson@co.clatsop.or.us>
2019-05-08 12:20:42 -0700

    no longer using clatsop_wm relying on on-the-fly projections

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ export const wgs84 = "EPSG:4326";
 export const wm = "EPSG:3857";
 
 export const myGeoServer = "https://geoserver.wildsong.biz/geoserver";
-export const workspace = "clatsop_wm";
+export const workspace = "clatsop";
 export const astoria_ll = [-123.834,46.187];
 
 export const usngPrecision = [
