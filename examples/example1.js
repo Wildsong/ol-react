4bd01255225a2c2b2ea38e5658139a68243afad9
Brian H Wilson <bwilson@co.clatsop.or.us>
2019-06-28 15:24:22 -0700

    Added more tests for ol-ext and refactored to use useState.

--- examples/example1.js
+++ examples/example1.js
@@ -1,17 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { ATTRIBUTION as osmAttribution } from 'ol/source/OSM'
-// Bootstrap (reactstrap in this case)
-import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    Button
-} from 'reactstrap'
 import SliderControl from './slider-control'
 import { Map, View, Feature, control, geom, interaction, layer } from '../src'
 import Select from 'react-select'
@@ -19,11 +8,27 @@ import { buildStyle } from '../src/style';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../App.css'
 
-import { transform } from 'ol/proj'
+// ol-ext css
+import 'ol-ext/dist/ol-ext.css'
+import 'style.css'
+import './css/fontmaki.css'
+import './css/fontmaki2.css'
+
+import { fromLonLat, transform } from 'ol/proj'
 import { myGeoServer, astoria_wm } from '../src/constants'
 
 const geoserverWMS = myGeoServer + "/wms?"
 const geoserverLayers = "taxlots"
+
+const initialGeoBookmarks = {
+    "Astoria": { pos: fromLonLat([-123.836,46.182]), zoom: 13, permanent:true },
+	"Cannon Beach": { pos: fromLonLat([-123.969,45.893]), zoom: 13, permanent:true },
+    "Gearhart": { pos: fromLonLat([-123.9188,46.026]), zoom: 13, permanent:true },
+    "Hammond": { pos: fromLonLat([-123.9520,46.2000]), zoom: 14, permanent:true },
+    "Jewell": { pos: fromLonLat([-123.5032,45.9345]), zoom: 14, permanent:true },
+    "Seaside": { pos: fromLonLat([-123.920,45.994]), zoom: 12, permanent:true },
+    "Warrenton": { pos: fromLonLat([-123.924,46.165]), zoom: 13, permanent:true },
+};
 
 const transformfn = (coordinates) => {
     for (let i = 0; i < coordinates.length; i+=2) {
@@ -54,60 +59,30 @@ const EventList = (props) => {
     );
 }
 
-export default class Example1 extends React.Component {
-    static propTypes = {
-        title: PropTypes.string
-    };
+/* ============================================================================= */
 
-    state = {
-        enableModify: true, // can't change this in the app yet
-        opacityOSM : 98,
-        opacityVector : 100,
-        typeIndex : 0, // index into typeSelect
-        events : [],
-        pointer : '',
-        markerId: 1,
-    }
+const Example1 =({ title }) => {
+    const [enableModify, setModify] = useState(true); // no button yet!
+    const [opacityOSM, setOpacityOSM] = useState(80);
+    const [opacityVector, setOpacityVector] = useState(100);
+    const [typeIndex, setTypeIndex] = useState(0); // index into draw typeSelect
+    const [pointer, setPointer] = useState('');
+    const [markerId, setMarker] = useState(1);
+    const [mapCenter, setMapCenter] = useState(astoria_wm);
 
-    changeOpacity1 = (value) => {
-        this.setState({opacityOSM : value});
-    }
-
-    changeOpacity2 = (value) => {
-        this.setState({opacityVector : value});
-    }
-
-    changeType = (o) => {
-        this.setState({ typeIndex : o.index });
-    }
-
-    handleMapEvent = (e) => {
-        if (this.state.events.length > 5) {
-            this.state.events.shift();
-        }
-        this.state.events.push(e.type);
-        this.setState({
-            events : this.state.events,
-            markerId : ++this.state.markerId,
-        });
-        //event.stopPropagation(); // this stops draw interaction
-    }
-
-    handlePointerMove = (e) => {
-//        console.log(e.type);
-        this.setState({ pointer: e.coordinate });
-        //event.stopPropagation(); // this stops the other events!
-    }
-
-    handleAddFeature = (e) => {
+    const handleAddFeature = (e) => {
         console.log("handleAddFeature", e, e.feature);
     }
 
-// This version makes ALL the point markers increment at the same time.
-    clickMarker = (feature, resolution) => {
+    const handleMapEvent = (e) => {
+        console.log("Map event", e);
+    }
+
+// This version makes ALL the point markers increment at the same time. Unfortunately
+    const clickMarker = (feature, resolution) => {
         const s = buildStyle({
             text: {
-                text: this.state.markerId.toString(),
+                text: markerId.toString(),
                 offsetY: -10,
             },
         //  currently this draws a blue 5 pointed star
@@ -123,160 +98,173 @@ export default class Example1 extends React.Component {
         return s;
     }
 
-    render() {
-        let pointStyle = {
-            image: {
-                type: 'circle',
-                radius: 4,
-                fill: { color: [100,100,100, 0.5] },
-                stroke: { color: 'green', width: 1 }
-            }
-        };
-        let multipointStyle = {
-            image: {
-                type: 'circle',
-                radius: 4,
-                fill: { color: [0,0,255, 0.4] },
-                stroke: { color: 'red', width: 1 }
-            }
-        };
-        let lineStyle = {
-            stroke: {
-                color: [255, 255, 0, 1],
-                width: 3
-            }
-        };
-        let polyStyle = {
-            stroke: {color: [0, 0, 0, 1], width:4},
-            fill: {color: [255, 0, 0, .250]},
-        };
+    const pointStyle = {
+        image: {
+            type: 'circle',
+            radius: 4,
+            fill: { color: [100,100,100, 0.5] },
+            stroke: { color: 'green', width: 1 }
+        }
+    };
+    const multipointStyle = {
+        image: {
+            type: 'circle',
+            radius: 4,
+            fill: { color: [0,0,255, 0.4] },
+            stroke: { color: 'red', width: 1 }
+        }
+    };
+    const lineStyle = {
+        stroke: {
+            color: [255, 255, 0, 1],
+            width: 3
+        }
+    };
+    const polyStyle = {
+        stroke: {color: [0, 0, 0, 1], width:4},
+        fill: {color: [255, 0, 0, .250]},
+    };
 
-        return (
-            <>
-                <h2>{ this.props.title }</h2>
-                Tile source: OpenStreetMap<br />
-                WMS source: taxlots from GeoServer
-                <h4>Vector source</h4>
-                    <ul>
-                        <li> Point: small green circle near Astor Column</li>
-                        <li> Circle: big circle around Astoria </li>
-                        <li> LineString: yellow line near Astoria</li>
-                        <li> Polygon: triangle with a triangle hole inside it</li>
-                    </ul>
-
-                There are two vector layers, one display only
-                and one that can be edited.
-                Interactions: DRAW into "draw" layer.
-                Controls: Sliders, Full screen, Zoom (range 8...12)
-
-                <SliderControl
-                    onChange={ this.changeOpacity1 }
-                    title="OSM"
-                    value={ this.state.opacityOSM }
-                />
-                <SliderControl
-                    onChange={ this.changeOpacity2 }
-                    title="Vectors"
-                    value={ this.state.opacityVector }
-                />
-
-                FIXME I can change the Vector Type now but the style does not update
-                so after drawing (eg) a linestring, there is no defined line style so the line poof! disappears.
-                Not high on my priorities right now.
+    return (
+        <>
+            <h2>{ title }</h2>
+            Tile source: OpenStreetMap<br />
+            WMS source: taxlots from GeoServer
+            <h4>Vector source</h4>
+                <ul>
+                    <li> Point: small green circle near Astor Column</li>
+                    <li> Circle: big circle around Astoria </li>
+                    <li> LineString: yellow line near Astoria</li>
+                    <li> Polygon: triangle with a triangle hole inside it</li>
+                </ul>
 
                 <p>
-                    Handling map events:
-                    pointermove, click, changesize, moveend
+                    There are two vector layers, one display only
+                    and one that can be edited.
+                    <br />
+                    Interactions: DRAW into "draw" layer.
+                    Controls: Sliders, Full screen, Zoom (range 8...12)
+                    Ol-ext controls: Geobookmarks
+                    Layer switcher (doesn't quite work)
+                    Search Nominatim
                 </p>
 
-                <Map
-                    view=<View zoom={ 10 } center={ astoria_wm }
-                            minZoom={9} maxZoom={19} />
-                    useDefaultControls={ false }
+            <SliderControl title="OSM" onChange={ setOpacityOSM } value={ opacityOSM } />
+            <SliderControl title="Vectors" onChange={ setOpacityVector } value={ opacityVector } />
 
-                    onPointerMove={ this.handlePointerMove }
-                    onChangeSize={ this.handleMapEvent }
-                    onMoveEnd={ this.handleMapEvent }
-                >
-                    <control.FullScreen />
-                    <control.Zoom />
+            FIXME I can change the Vector Type now but the style does not update
+            so after drawing (eg) a linestring, there is no defined line style so the line poof! disappears.
+            Not high on my priorities right now.
 
-                    <layer.Tile source="OSM"
-                        attributions={ attributions }
-                        opacity={ this.state.opacityOSM/100 }
-                    />
+            <p>
+                Handling map events:
+                pointermove, click, changesize, moveend
+            </p>
 
-                    <layer.Tile source="WMS"
-                        url={ geoserverWMS }
-                        params={{LAYERS: geoserverLayers,
-                            STYLES: "redline", // WMS style, from GeoServer in this case
-                            TILED: true}}
-                    />
+            <Map
+                view=<View zoom={ 10 } center={ mapCenter } minZoom={9} maxZoom={19} />
+                useDefaultControls={ false }
 
-                    <layer.Vector name="Display" opacity={ this.state.opacityVector/100 } >
-                        <Feature id="test-line" style={ lineStyle }>
-                            <geom.LineString transform={transformfn} layout="XY">
-                                { [[6000,6000], [-6000, 6000], [-6000, 6000], [-6000, -6000], [6000,-6000]] }
-                            </geom.LineString>
-                        </Feature>
+                onPointerMove={ (e) => { setPointer(e.coordinate); } }
+                onChangeSize={ handleMapEvent }
+                onMoveEnd={ handleMapEvent }
+            >
+                <control.FullScreen />
+                <control.Zoom />
 
-                        <Feature id="test-circle" style={ pointStyle }>
-                            <geom.Circle modify={ this.state.enableModify } >{[astoria_wm, 100]}</geom.Circle>
-                        </Feature>
+                <control.LayerPopupSwitcher />
+                <control.GeoBookmarkControl marks={ initialGeoBookmarks }/>
+                <control.SearchNominatim onSelect={ (e)=>{console.log("selected feature:",e);} }/>
 
-                        <Feature id="test-circle-zeroradius" style={ polyStyle }>
-                            <geom.Circle transform={ transformfn } >{[6000,0]}</geom.Circle>
-                        </Feature>
-
-                        <Feature id="test-polygon" style={ polyStyle }>
-                            <geom.Polygon transform={ transformfn }>
-                                {[
-                                    [[-3500, -2000], [3500, -2000], [0, 4000], [-3500, -2000]],
-                                    [[0, -1000], [1000, 1000], [-1000, 1000], [0, -1000]],
-                                ]}
-                            </geom.Polygon>
-                        </Feature>
-
-                        <Feature id="test-point" style={ pointStyle }>
-                            <geom.Point transform={ transformfn } >
-                                {[1835, -910]}
-                            </geom.Point>
-                        </Feature>
-
-                        <Feature id="test-multipoint" style={ multipointStyle }>
-                            <geom.MultiPoint transform={ transformfn } >
-                                { [[-6000, -4000], [6000, -3000], [0, 6400]] }
-                            </geom.MultiPoint>
-                        </Feature>
-                    </layer.Vector>
-
-                    <layer.Vector name="Draw" style={ this.clickMarker }
-                        addfeature={ this.handleAddFeature }>
-                        <interaction.Draw type={ typeSelect[this.state.typeIndex].label }
-                            drawend={ this.handleAddFeature }
-                        />
-                    </layer.Vector>
-                </Map>
-
-                <EventList events={ this.state.events }/>
-                <p> { this.state.pointer[0] + ', ' + this.state.pointer[1] } </p>
-                Select vector type to draw
-                <Select
-                    className="select"
-                    defaultValue={ typeSelect[ 0 ] }
-                    options={ typeSelect }
-                    onChange={ this.changeType }
+                <layer.Tile source="Stamen"
+                    title="Toner"
+                    attributions={ attributions } layer="toner"
+                    //visible={ true }
+                    baseLayer={ true }
                 />
 
-                Implement and test...
-                <ul>
-                    <li> MultiLineString </li>
-                    <li> MultiPolygon</li>
-                    <li> GeometryCollection</li>
-                    <li> Animation</li>
-                    <li> Overlay</li>
-                </ul>
-            </>
-        );
-    }
+                <layer.Tile source="OSM"
+                    title="OpenStreetMap"
+                    attributions={ attributions }
+                    //visible={ false }
+                    baseLayer={ true }
+                />
+
+                <layer.Tile source="WMS"
+                    title="Taxlots"
+                    url={ geoserverWMS }
+                    params={{LAYERS: geoserverLayers,
+                        STYLES: "redline", // WMS style, from GeoServer in this case
+                        TILED: true}}
+                />
+
+                <layer.Vector title="Vector Shapes" opacity={ opacityVector/100 } >
+                    <Feature id="test-line" style={ lineStyle }>
+                        <geom.LineString transform={transformfn} layout="XY">
+                            { [[6000,6000], [-6000, 6000], [-6000, 6000], [-6000, -6000], [6000,-6000]] }
+                        </geom.LineString>
+                    </Feature>
+
+                    <Feature id="test-circle" style={ pointStyle }>
+                        <geom.Circle modify={ enableModify } >{[astoria_wm, 100]}</geom.Circle>
+                    </Feature>
+
+                    <Feature id="test-circle-zeroradius" style={ polyStyle }>
+                        <geom.Circle transform={ transformfn } >{[6000,0]}</geom.Circle>
+                    </Feature>
+
+                    <Feature id="test-polygon" style={ polyStyle }>
+                        <geom.Polygon transform={ transformfn }>
+                            {[
+                                [[-3500, -2000], [3500, -2000], [0, 4000], [-3500, -2000]],
+                                [[0, -1000], [1000, 1000], [-1000, 1000], [0, -1000]],
+                            ]}
+                        </geom.Polygon>
+                    </Feature>
+
+                    <Feature id="test-point" style={ pointStyle }>
+                        <geom.Point transform={ transformfn } >
+                            {[1835, -910]}
+                        </geom.Point>
+                    </Feature>
+
+                    <Feature id="test-multipoint" style={ multipointStyle }>
+                        <geom.MultiPoint transform={ transformfn } >
+                            { [[-6000, -4000], [6000, -3000], [0, 6400]] }
+                        </geom.MultiPoint>
+                    </Feature>
+                </layer.Vector>
+
+                <layer.Vector title="Draw" style={ clickMarker }
+                    addfeature={ handleAddFeature }>
+                    <interaction.Draw type={ typeSelect[typeIndex].label }
+                        drawend={ handleAddFeature }
+                    />
+                </layer.Vector>
+            </Map>
+
+            <p> { pointer[0] + ', ' + pointer[1] } </p>
+            Select vector type to draw
+            <Select
+                className="select"
+                defaultValue={ typeSelect[0] }
+                options={ typeSelect }
+                onChange={ (o) => { setTypeIndex(o.index); console.log("You selected ", typeIndex); } }
+            />
+
+            Implement and test...
+            <ul>
+                <li> MultiLineString </li>
+                <li> MultiPolygon</li>
+                <li> GeometryCollection</li>
+                <li> Animation</li>
+                <li> Overlay</li>
+            </ul>
+        </>
+    );
 }
+Example1.propTypes = {
+    title: PropTypes.string
+};
+
+export default Example1;
