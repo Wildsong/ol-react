3e66fa576032bab571ef8339d3a665333be5dd83
Brian H Wilson <brian@wildsong.biz>
2019-08-04 21:51:35 -0700

    comments

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
+import {render} from 'react-dom'
 import App from './App'
-import configureStore from './configure-store'
 
+// We need redux here because of redux-first-router
+import {Provider} from 'react-redux'
+import configureStore from './configure-store'
 const { store } = configureStore();
 
 render(
