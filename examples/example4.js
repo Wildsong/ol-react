c590229fc5b5c2790c9f462e061be3ae0a4686c9
Brian H Wilson <brian@wildsong.biz>
2019-08-11 09:42:41 -0700

    fixed a style typo, added more services

--- examples/example4.js
+++ examples/example4.js
@@ -5,7 +5,8 @@ import {MapProvider} from '../src/map-context' // eslint-disable-line no-unused-
 
 import {Map as olMap, View as olView} from 'ol'
 import {fromLonLat} from 'ol/proj'
-import {Stroke} from 'ol/style'
+import Style from 'ol/style/Style'
+import {Stroke, Fill} from 'ol/style'
 
 import {astoria_ll} from './constants'
 const DEFAULT_CENTER = astoria_ll;
@@ -14,9 +15,27 @@ const DEFAULT_ZOOM = 13;
 const bingmaps_key = (process.env.BINGMAPS_KEY !== undefined)? process.env.BINGMAPS_KEY : "";
 
 // Supported formats: JSON, GeoJSON, PBF
-const featureServer = "https://services.arcgis.com/uUvqNMGPm7axC2dD/arcgis/rest/services/Oregon_Zoning_2017/FeatureServer/0"
-//&returnExceededLimitFeatures=false&maxRecordCountFactor=3&outSR=102100&resultType=tile
-//&quantizationParameters=%7B%22mode%22%3A%22view%22%2C%22originPosition%22%3A%22upperLeft%22%2C%22tolerance%22%3A1222.992452562501%2C%22extent%22%3A%7B%22xmin%22%3A-14401959.121378995%2C%22ymin%22%3A5635549.22141099%2C%22xmax%22%3A-13775786.985666994%2C%22ymax%22%3A6261721.35712299%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%2C%22latestWkid%22%3A3857%7D%7D%7D"
+const oregonAGOL = "https://services.arcgis.com/uUvqNMGPm7axC2dD/arcgis/rest/services"
+const zoning2017FeatureServer = oregonAGOL + "/Oregon_Zoning_2017/FeatureServer/0";   // 2017
+const zoning2014FeatureServer = oregonAGOL + "/Zoning/FeatureServer/0";  // 2014
+const zoningStyle = new Style({
+    stroke: new Stroke({color: [0, 0, 0, 1], width:1}),
+    fill: new Fill({color: [76,129,205, .75]}),
+});
+
+const tileServer = 'https://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Specialty/ESRI_StateCityHighway_USA/MapServer';
+
+const dogamiServer = "https://gis.dogami.oregon.gov/arcgis/rest/services/Public"
+const dogamiLandslideUrl = dogamiServer + "/Landslide_Susceptibility/ImageServer"
+const dogamiSlidoUrl = dogamiServer + "/SLIDO3_4/MapServer"
+
+// FEMA https://hazards.fema.gov/femaportal/wps/portal/NFHLWMS
+const FEMA_NFHL_arcgisREST = "https://hazards.fema.gov/gis/nfhl/rest/services/public/NFHL/MapServer"
+const femaPLSSUrl = FEMA_NFHL_arcgisREST + '/5';
+const plssStyle = new Style({
+    stroke: new Stroke({color: [0, 0, 0, 1], width:1}),
+    //fill: new Fill({color: [255, 0, 0, .250]}),
+});
 
 const Example4 = () => {
     const [theMap] = useState(new olMap({
@@ -33,10 +52,6 @@ const Example4 = () => {
     const toggleLayer = () => {
         setBingVisible(!bingVisible);
     }
-    const polyStyle = new Stroke({
-        stroke: new Stroke({color: [0, 255, 0, 1], width:2}),
-        //fill: new Fill({color: [255, 0, 0, .250]}),
-    });
 
     return (
         <>
@@ -59,16 +74,38 @@ const Example4 = () => {
                     <layer.Tile title="Bing Road" baseLayer={true}>
                         <source.BingMaps imagerySet="CanvasLight" apikey={bingmaps_key}/>
                     </layer.Tile>
+
                     <layer.Tile title="Bing Aerial" visible={bingVisible} baseLayer={true}>
                         <source.BingMaps imagerySet="Aerial" apikey={bingmaps_key}/>
                     </layer.Tile>
-                    <layer.Vector title="Oregon Zoning">
-                        <source.JSON url={featureServer} loader="esrijson" style={polyStyle}/>
+
+                    <layer.Tile title="ESRI StateCityHighway USA" visible={false}>
+                        <source.TileArcGISRest url={tileServer} />
+                    </layer.Tile>
+
+                    <layer.Image title="DOGAMI Landslide Susceptibility">
+                        <source.ImageArcGISRest url={dogamiLandslideUrl}/>
+                    </layer.Image>
+
+                    <layer.Image title="DOGAMI Slides">
+                        <source.ImageArcGISRest url={dogamiSlidoUrl}/>
+                    </layer.Image>
+
+                    <layer.Vector title="Oregon Zoning 2017" style={zoningStyle}>
+                    <source.JSON url={zoning2017FeatureServer} loader="esrijson"/>
+                    </layer.Vector>
+
+                    <layer.Vector title="Oregon Zoning 2014" style={zoningStyle}>
+                        <source.JSON url={zoning2014FeatureServer} loader="esrijson"/>
+                    </layer.Vector>
+
+                    <layer.Vector title="PLSS (FEMA)" style={plssStyle}>
+                    <source.JSON url={femaPLSSUrl} loader="esrijson"/>
                     </layer.Vector>
 
                     <control.ScaleLine units={control.ScaleLineUnits.US} />
                 </Map>
-                <control.LayerSwitcher/>
+                <control.LayerSwitcher collapsed={false}/>
                 <control.ScaleLine units={control.ScaleLineUnits.METRIC} />
                 <control.Attribution />
                 </MapProvider>
