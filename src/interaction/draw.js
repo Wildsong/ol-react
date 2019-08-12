0c64b333af16ffa14f63fd74d89134773bbe50f1
Brian H Wilson <brian@wildsong.biz>
2019-08-11 22:05:09 -0700

    trying to make draw interaction change types effectively

--- src/interaction/draw.js
+++ src/interaction/draw.js
@@ -2,47 +2,55 @@ import React, {useState, useContext, useEffect} from 'react';  // eslint-disable
 import PropTypes from 'prop-types'
 import {MapContext} from '../map-context'
 import {SourceContext} from '../source-context'
-//import {GeometryType as olGeometryType} from 'ol/geom'
 import {Style as olStyle} from 'ol/style'
 import {Draw as olDraw} from 'ol/interaction'
 
 const Draw = (props) => {
     const map = useContext(MapContext);
     const source = useContext(SourceContext);
-    //const olEvents = ["drawend", "drawstart"];
+    const [interaction, setInteraction] = useState(new olDraw({
+        ...props,
+        source
+    }));
 
     useEffect(() => {
-        const interaction = new olDraw({
-            ...props,
-            source
-        });
         interaction.addEventListener("drawend", (evt) => {
-            if (typeof props.drawend !== 'undefined') {
+            if (props.drawend !== undefined) {
                 props.drawend(evt);
             }
         });
 
         map.addInteraction(interaction);
+
+        console.log("Setting draw type to:", props.type);
+
         return () => {
             map.removeInteraction(interaction);
         }
     }, []);
 
-/*
-This did not work.
     useEffect(() => {
-        console.log("Draw type changed", draw, props);
-        draw.setProperties({type:props.type});
+        if (props.type !== interaction.get('type')) {
+            map.removeInteraction(interaction);
+            setInteraction(new olDraw({
+                ...props,
+                source
+            }))
+            console.log("Draw type changed", props, interaction);
+            map.addInteraction(interaction);
+        }
     }, [props.type]);
-*/
+
     return null;
 }
 Draw.propTypes = {
     active: PropTypes.bool,
-    type: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(['Point', 'LineString', 'LinearRing',
+        'Polygon', 'MultiPoint', 'MultiLineString', 'MultiPolygon',
+        'GeometryCollection', 'Circle']).isRequired,
     condition: PropTypes.func,
-    drawend: PropTypes.func,
     drawstart: PropTypes.func,
+    drawend: PropTypes.func,
     maxPoints: PropTypes.number,
     minPoints: PropTypes.number,
     style: PropTypes.oneOfType([PropTypes.func,
