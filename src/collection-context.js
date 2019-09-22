9acde57cc3377492e6c8957d9d3462731d7be3fd
Brian H Wilson <brian@wildsong.biz>
2019-09-21 21:38:45 -0700

    left out

--- src/collection-context.js
+++ src/collection-context.js
@@ -1,27 +1,15 @@
-"use strict";
+import React from 'react';  // eslint-disable-line no-unused-vars
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.CollectionProvider = exports.CollectionContext = void 0;
-
-var _react = _interopRequireDefault(require("react"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-// eslint-disable-line no-unused-vars
 // Currently I use this to create groupings of layers,
 // in theory it works for any collection (?)
 // https://upmostly.com/tutorials/how-to-use-the-usecontext-hook-in-react
-var CollectionContext = _react["default"].createContext();
 
-exports.CollectionContext = CollectionContext;
+const CollectionContext = React].createContext();
 
-var CollectionProvider = function CollectionProvider(props) {
-  return _react["default"].createElement(CollectionContext.Provider, {
-    value: props.collection
-  }, props.children);
-};
+const CollectionProvider = (props) => (
+    <CollectionContext.Provider collection={props.collection}>
+        {props.children}
+    </CollectionContext.Provider>
+)
 
-exports.CollectionProvider = CollectionProvider;
-//# sourceMappingURL=collection-context.js.map
+export {CollectionContext, CollectionProvider}
