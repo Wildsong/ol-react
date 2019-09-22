33f1f5426bb2c086cb69c267fb5da7e4fadac197
Brian H Wilson <brian@wildsong.biz>
2019-09-21 21:43:38 -0700

    new interaction

--- src/collection-context.js
+++ src/collection-context.js
@@ -4,7 +4,7 @@ import React from 'react';  // eslint-disable-line no-unused-vars
 // in theory it works for any collection (?)
 // https://upmostly.com/tutorials/how-to-use-the-usecontext-hook-in-react
 
-const CollectionContext = React].createContext();
+const CollectionContext = React.createContext();
 
 const CollectionProvider = (props) => (
     <CollectionContext.Provider collection={props.collection}>
